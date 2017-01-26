9e4c0c46ef2449d058c1de6a4b27f6a1b00ffd0d
Adam Detrick <akdetrick@gmail.com>
2017-01-26 17:51:35 -0500

    simplify build; set up sassdoc

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,60 +1,41 @@
 module.exports = function(grunt) {
 
-	grunt.loadNpmTasks('grunt-exec');
+	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-sassdoc');
 	grunt.loadNpmTasks('grunt-gh-pages');
-	grunt.loadNpmTasks('grunt-contrib-clean');
-	grunt.loadNpmTasks('grunt-preprocess');
-
-	var DIR_DOC_SRC = 'docs/',
-		DIR_BUILD = DIR_DOC_SRC + 'build/';
 
 	grunt.initConfig({
 
 		// config variables -------------------------------------
 		package: grunt.file.readJSON('package.json'),
-		fontUrl: 'http://static2.meetupstatic.com/fonts/graphik.css',
-		sassquatchCSSPath: 'bundle/sassquatch.css',
 		// ------------------------------------------------------
 
-		'exec': {
-			webpack: 'webpack',
-			sassdocjs: [
-				'sassdocjs sass/',
-				'-v <%= package.verson %>',
-			].join(' ')
-		},
-		'hologram': {
-			generate: {
+		'sassdoc': {
+			default: {
+				src: 'scss/**/*.scss'
 				options: {
-					config: DIR_DOC_SRC+'/config.yml'
+					theme: 'neat'
 				}
+			}
+		},
+		'sass': {
+			dist: {
+				files: {
+					'sassdoc/assets/css/swarm-sassutils.css': 'scss/main.scss'
+				}
+			},
+			options: {
+				sourceMaps: true
 			}
 		},
 		'gh-pages': {
 			options: {
-				base: DIR_BUILD
+				base: 'sassdoc/'
 			},
 			src: ['**']
-		},
-		'clean': {
-			docs: [DIR_BUILD],
-			css: [DIR_DOC_SRC + 'templates/css/sassquatch.css']
-		},
-		'preprocess': {
-			inline: {
-				src: [ 'docs/build/*.html', 'docs/build/pages/*.html' ],
-				options: {
-					inline: true,
-					context: {
-						DEBUG: false,
-						'FONT_URL': '<%= fontUrl %>',
-						'SASSQUATCH_CSS': '<%= sassquatchCSSPath %>',
-					}
-				}
-			}
 		}
 	});
 
-	grunt.registerTask('default', ['clean', 'exec', 'hologram', 'preprocess']);
+	grunt.registerTask('default', ['sassdoc', 'sass']);
 	grunt.registerTask('ghpages', ['default', 'gh-pages']);
 };
