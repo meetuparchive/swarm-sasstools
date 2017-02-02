453f474f2f90b67b076a9a6b6ecf51725a912095
Adam Detrick <akdetrick@gmail.com>
2017-02-01 19:23:24 -0500

    set up webpack config and dependencies; update grunt build

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var path = require('path');
+
+var ExtractPlugin = require('extract-text-webpack-plugin');
+
+var PATH_ENTRY = path.resolve(__dirname, 'docs', 'webpack-entry.scss');
+var PATH_BUNDLE_DEST = path.resolve(__dirname, 'docs', 'build', 'bundle');
+
+module.exports = {
+	module: {
+		loaders: [
+			{
+				test: /\.scss$/,
+				loader: ExtractPlugin.extract("css!sass")
+			}
+		]
+	},
+
+	plugins: [
+		new ExtractPlugin("swarm-sasstools.css")
+	],
+
+	// webpack requires a js entry point
+	// and bundle location
+	entry: {
+		sasstools: [PATH_ENTRY]
+	},
+	output: {
+		path: PATH_BUNDLE_DEST,
+		filename: 'bundle.js'
+	}
+};
