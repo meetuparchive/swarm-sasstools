39a42f27de7ac46b8d8e84ef838da55debb2a4f1
Adam Detrick <akdetrick@gmail.com>
2017-03-07 15:24:05 -0500

    fix for webpack2

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
 		loaders: [
 			{
 				test: /\.scss$/,
-				loader: ExtractPlugin.extract("css!sass")
+				loader: ExtractPlugin.extract("css-loader!sass-loader")
 			}
 		]
 	},
